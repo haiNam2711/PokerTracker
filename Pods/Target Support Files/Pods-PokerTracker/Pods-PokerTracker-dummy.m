c3d27e02215e8ff176593b2ba4037513fdfa9f00
hnam2711 <namcuong2711@gmail.com>
2024-04-09 09:56:09 +0700

    Init Pod Project

---
+++ Pods/Target Support Files/Pods-PokerTracker/Pods-PokerTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokerTracker : NSObject
+@end
+@implementation PodsDummy_Pods_PokerTracker
+@end
---
+++ PokerTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokerTracker
+//
+//  Created by Hai Nam on 9/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
