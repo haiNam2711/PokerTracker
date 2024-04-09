fee3da0a5a0bd8e343249b1ffe14b561ebc8968b
hnam2711 <namcuong2711@gmail.com>
2024-04-09 10:56:35 +0700

    Fix Helper Bug

--- PokerTracker/Helper/LocalDB/PlayerTable.swift
+++ PokerTracker/Helper/LocalDB/PlayerTable.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SQLite
+
+class PlayerTable {
+    static let players = Table("Player")
+    static let id = Expression<Int>("PlayerID")
+    static let name = Expression<String>("Name")
+    
+    static func insert(item: Player) throws {
+        let DB = LocalDB.shared.getDB()
+        let insert = players.insert(name <- item.name)
+        let rowId = try DB.run(insert)
+        if rowId <= 0 {
+            throw DBError(message: "error insert game item")
+        }
+    }
+    
+    static func findAll() throws -> [Player]? {
+        let DB = LocalDB.shared.getDB()
+        var retArray = [Player]()
+        let items = try DB.prepare(players)
+        for item in items {
+            retArray.append(Player(id: item[id], name: item[name]))
+        }
+        return retArray
+    }
+    
+}
