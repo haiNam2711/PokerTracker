322788df80fdb136995fc7d621ad11cfb9248160
hnam2711 <namcuong2711@gmail.com>
2024-04-09 16:31:18 +0700

    Fix not do catch game table

--- PokerTracker/Helper/LocalDB/GameTable.swift
+++ PokerTracker/Helper/LocalDB/GameTable.swift
@@ -18,31 +18,21 @@ class GameTable {
     static let fee = Expression<Int>("Fee")
     
     static func insert(item: Game) throws -> Game? {
-        do {
-            let DB = LocalDB.shared.getDB()
-            let insert = games.insert(time <- item.time, cashIn <- item.standardCashIn, chipOut <- item.standardChipOut, feeTypeInValue <- item.feeTypeInValue, fee <- item.fee)
-            let rowId = try DB.run(insert)
-            guard rowId > 0 else { return nil }
-            return Game(id: Int(rowId), time: item.time, standardCashIn: item.standardCashIn, standardChipOut: item.standardChipOut, feeTypeInValue: item.feeTypeInValue, fee: item.fee)
-        } catch {
-            print("error insert game item: \(error)")
-            return nil
-        }
+        let DB = LocalDB.shared.getDB()
+        let insert = games.insert(time <- item.time, cashIn <- item.standardCashIn, chipOut <- item.standardChipOut, feeTypeInValue <- item.feeTypeInValue, fee <- item.fee)
+        let rowId = try DB.run(insert)
+        guard rowId > 0 else { return nil }
+        return Game(id: Int(rowId), time: item.time, standardCashIn: item.standardCashIn, standardChipOut: item.standardChipOut, feeTypeInValue: item.feeTypeInValue, fee: item.fee)
     }
     
     static func findAll() throws -> [Game]? {
-        do {
-            let DB = LocalDB.shared.getDB()
-            var retArray = [Game]()
-            let items = try DB.prepare(games)
-            for item in items {
-                retArray.append(Game(id:item[id], time: item[time], standardCashIn: item[cashIn], standardChipOut: item[chipOut], feeTypeInValue: item[feeTypeInValue], fee: item[fee]))
-            }
-            return retArray
-        } catch {
-            print("Error games: \(error)")
-            return nil
+        let DB = LocalDB.shared.getDB()
+        var retArray = [Game]()
+        let items = try DB.prepare(games)
+        for item in items {
+            retArray.append(Game(id:item[id], time: item[time], standardCashIn: item[cashIn], standardChipOut: item[chipOut], feeTypeInValue: item[feeTypeInValue], fee: item[fee]))
         }
+        return retArray
     }
     
 }
